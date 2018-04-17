952479c0dacd4995c647455126243cf6b838111b
Ashtell <mhallisawesome@gmail.com>
2018-04-17 03:14:39 +0000

    Created Pages controller.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+#contains all the code for any page inside /pages
+class PagesController < ApplicationController
+  #backend code for pages/index
+  def index
+  end
+  #backend code for pages/home
+  def home
+  end
+  #backend code for pages/profile
+  def profile
+  end
+  #backend code for pages/explore
+  def explore
+  end
+end
